45922f60335d477a8437bfe33ddf42807262ad62
Huy Le <huyle333@bu.edu>
2013-12-08 20:51:07 -0500

    test php for now...

---
+++ coursein.php
@@ -0,0 +1,5 @@
+<?php
+
+$q = $db->query("SELECT * FROM `courses` WHERE `number` = '".$phone."' AND `college` = '".$college."' AND `department` = '".$dept."' AND `course` = '".$num."' AND `section` = '".$section."'");
+
+?>
